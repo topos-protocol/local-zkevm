355a5c6c34d6670cadd0ac7ac6ccba58ba2ccff7
Sébastien Dan <sebastien.dan@gmail.com>
2023-12-01 10:56:54 +0100

    refactor: use hardhat HRE

--- sample-hardhat-project/hardhat.config.ts
+++ sample-hardhat-project/hardhat.config.ts
@@ -1,10 +1,18 @@
-import { extendEnvironment, HardhatUserConfig } from "hardhat/config";
-
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-tracer";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+// import 'hardhat-tracer'
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.19",
-};
+  defaultNetwork: 'erigon',
+  networks: {
+    erigon: {
+      url: 'http://127.0.0.1:8545',
+      accounts: [
+        '0x26e86e45f6fc45ec6e2ecd128cec80fa1d1505e5507dcd2ae58c3130a7a97b48',
+      ],
+    },
+  },
+  solidity: '0.8.19',
+}
 
-export default config;
+export default config
