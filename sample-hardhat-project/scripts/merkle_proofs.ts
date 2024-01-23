cc86f66c0534610186b322a45ae6b5f71e3e3e13
Jawad Tariq <sjcool420@hotmail.co.uk>
2024-01-23 14:11:51 -0500

    fix: refactor
    Signed-off-by: Jawad Tariq <sjcool420@hotmail.co.uk>

--- sample-hardhat-project/scripts/merkle_proofs.ts
+++ sample-hardhat-project/scripts/merkle_proofs.ts
@@ -2,23 +2,20 @@ import { Block, hexlify, JsonRpcProvider, TransactionResponse } from 'ethers'
 import { RLP } from '@ethereumjs/rlp'
 import { Trie } from '@ethereumjs/trie'
 
-/// Generate a Merkle Patricia Tree proof for a transaction
-/// @param tx Transaction to generate the proof for
-/// @param provider Provider to use to fetch the block
-/// @throws If the block's receipts root does not match the trie root
-/// @returns The transaction's Merkle Patricia Trie proof
+/**
+ * Generate a merkle proof for a transaction's receipt
+ *
+ * @param tx Transaction to generate the proof for
+ * @param provider Provider to use to fetch the transaction information
+ * @returns The receipt's merkle proof
+ */
 export async function generateReceiptMptProof(
   tx: TransactionResponse,
   provider: JsonRpcProvider
 ) {
-  const prefetchTxs = true
-  const block = await provider.getBlock(tx.blockHash!, prefetchTxs)
-  const rawBlock = await (provider as any).send('eth_getBlockByHash', [
-    tx.blockHash,
-    prefetchTxs,
-  ])
+  const { block, indexOfTx } = await fetchAndFindTransaction(tx, provider)
+  const receiptsRoot = await getReceiptRoot(tx.blockHash!, provider)
 
-  const receiptsRoot = rawBlock.receiptsRoot
   const trie = await createReceiptMpt(block!)
   const trieRoot = trie.root()
   if ('0x' + trieRoot.toString('hex') !== receiptsRoot) {
@@ -28,53 +25,79 @@ export async function generateReceiptMptProof(
        receiptsRoot: "${receiptsRoot}"`
     )
   }
-
-  const indexOfTx = block!.prefetchedTransactions.findIndex(
-    (_tx) => _tx.hash === tx.hash
-  )
   const key = Buffer.from(RLP.encode(indexOfTx))
-  const proof = await trie.createProof(key)
-  const proofStringArray = proof.map((p) => hexlify(p))
-  return proofStringArray
+  const proof = (await trie.createProof(key)).map((p) => hexlify(p)).toString()
+  return proof
 }
 
-/// Verify a Merkle Patricia Tree proof for a transaction
-/// @param tx Transaction to verify the proof for
-/// @param provider Provider to use to fetch the block
-/// @param proof Proof to verify
-/// @returns Whether the proof is valid
+/**
+ * Verify a merkle proof for a transaction's receipt
+ *
+ * @param tx Transaction to verify the proof for
+ * @param provider Provider to use to fetch the transaction information
+ * @param proof The receipt's merkle proof
+ * @returns Whether the proof is valid or not
+ */
 export async function verifyReceiptMptProof(
   tx: TransactionResponse,
   provider: JsonRpcProvider,
-  proof: string[]
+  proof: string
+) {
+  const { indexOfTx } = await fetchAndFindTransaction(tx, provider)
+
+  const receiptsRoot = await getReceiptRoot(tx.blockHash!, provider)
+  const receiptRootBuffer = Buffer.from(receiptsRoot.slice(2), 'hex')
+
+  const key = Buffer.from(RLP.encode(indexOfTx))
+
+  const proofStringArray = proof.split(',')
+  const proofBufferArray: Buffer[] = []
+  proofStringArray.forEach((p) =>
+    proofBufferArray.push(Buffer.from(p.slice(2), 'hex'))
+  )
+
+  const trie = new Trie()
+  const value = await trie.verifyProof(receiptRootBuffer, key, proofBufferArray)
+  return value !== null
+}
+
+/**
+ * Fetches the block and finds the index of the transaction within the block
+ *
+ * @param tx - The transaction to find
+ * @param provider - Provider to use to fetch the transaction information
+ * @returns An object containing the block and the index of the transaction
+ */
+async function fetchAndFindTransaction(
+  tx: TransactionResponse,
+  provider: JsonRpcProvider
 ) {
   const prefetchTxs = true
   const block = await provider.getBlock(tx.blockHash!, prefetchTxs)
   const indexOfTx = block!.prefetchedTransactions.findIndex(
     (_tx) => _tx.hash === tx.hash
   )
-  const rawBlock = await (provider as any).send('eth_getBlockByHash', [
-    tx.blockHash,
-    prefetchTxs,
-  ])
-
-  const receiptsRoot = rawBlock.receiptsRoot
-  const receiptRootBuffer = Buffer.from(receiptsRoot.slice(2), 'hex')
-  const key = Buffer.from(RLP.encode(indexOfTx))
-  let proofBufferArray: Buffer[] = []
-  proof.forEach((p) => proofBufferArray.push(Buffer.from(p.slice(2), 'hex')))
-
-  const trie = new Trie()
-  const value = await trie.verifyProof(receiptRootBuffer, key, proofBufferArray)
-  if (value === null) {
-    return false
-  }
-  return true
+  return { block, indexOfTx }
 }
 
-/// Create a Merkle Patricia Tree for a block's receipts
-/// @param block Block to create the trie for
-/// @returns The receipt's Merkle Patricia Trie
+/**
+ * Get the block's receipts root
+ *
+ * @param blockHash Hash of the block to get the receipts root for
+ * @param provider Provider to use to fetch the transaction information
+ * @returns The block's receipts root
+ */
+export async function getReceiptRoot(blockHash: string, provider: JsonRpcProvider) {
+  const rawBlock = await provider.send('eth_getBlockByHash', [blockHash, true])
+  return rawBlock.receiptsRoot
+}
+
+/**
+ * Create a Merkle Patricia Tree for a block's receipts
+ *
+ * @param block Block to create the trie for
+ * @returns The block's Merkle Patricia Trie
+ */
 async function createReceiptMpt(block: Block) {
   const trie = new Trie()
   await Promise.all(
@@ -83,27 +106,17 @@ async function createReceiptMpt(block: Block) {
       const { cumulativeGasUsed, logs, logsBloom, type, status } = receipt!
 
       const key = Buffer.from(RLP.encode(index))
-      const value = Buffer.from(
-        RLP.encode([
-          status,
-          Number(cumulativeGasUsed),
-          logsBloom,
-          logs.map((l) => [l.address, l.topics as string[], l.data]),
-        ])
-      )
+      const value = RLP.encode([
+        status,
+        Number(cumulativeGasUsed),
+        logsBloom,
+        logs.map((l) => [l.address, l.topics as string[], l.data]),
+      ])
 
       if (type === 0) {
-        trie.put(key, value)
+        trie.put(key, Buffer.from(value))
       } else {
-        const buf = Buffer.concat([
-          Buffer.from([type]),
-          RLP.encode([
-            status,
-            Number(cumulativeGasUsed),
-            logsBloom,
-            logs.map((l) => [l.address, l.topics as string[], l.data]),
-          ]),
-        ])
+        const buf = Buffer.concat([Buffer.from([type]), value])
         trie.put(key, buf)
       }
     })
