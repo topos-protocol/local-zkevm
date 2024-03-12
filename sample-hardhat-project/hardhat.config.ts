d9e4d388ff8153b91719e14642b4a126e63181e0
Marko Atanasievski <markoatana@gmail.com>
2024-03-12 13:19:21 +0100

    feat: quorum settings

--- sample-hardhat-project/hardhat.config.ts
+++ sample-hardhat-project/hardhat.config.ts
@@ -5,9 +5,9 @@ const config: HardhatUserConfig = {
   defaultNetwork: 'erigon',
   networks: {
     erigon: {
-      url: 'http://127.0.0.1:8545',
+      url: 'http://127.0.0.1:22000',
       accounts: [
-        '0x26e86e45f6fc45ec6e2ecd128cec80fa1d1505e5507dcd2ae58c3130a7a97b48',
+        '0xca3a6f8b83ed5876201605ae8507490d0a0205c0748e6376ed9661c9fecb98d7',
       ],
     },
   },
